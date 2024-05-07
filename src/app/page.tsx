00d4527991627ae69a595f826f0363dfe7a1a8a2
tremayne97 <callum.tremayne@gmail.com>
2024-05-07 16:27:43 +0100

    added ability to query by tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,9 @@ import { Room } from "@/db/schema";
 import { GithubIcon } from "lucide-react";
 import { getRooms } from "@/services/rooms";
 import TagsList, { splitTags } from "@/components/tags-list";
+import { SearchBar } from "./search-bar";
 
 function RoomCard({ room }: { room: Room }) {
-
   return (
     <Card className="flex flex-col justify-between">
       <CardHeader>
@@ -22,7 +22,7 @@ function RoomCard({ room }: { room: Room }) {
         <CardDescription>{room.description}</CardDescription>
       </CardHeader>
       <CardContent className="flex gap-1">
-      <TagsList tags={splitTags(room.tags)} />
+        <TagsList tags={splitTags(room.tags)} />
       </CardContent>
       <CardFooter className="flex justify-between">
         {room.githubRepo && (
@@ -44,8 +44,12 @@ function RoomCard({ room }: { room: Room }) {
   );
 }
 
-export default async function Home() {
-  const rooms = await getRooms();
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { search: string };
+}) {
+  const rooms = await getRooms(searchParams.search);
   return (
     <main className="min-h-screen p-16">
       <div className="flex justify-between items-center mb-8">
@@ -54,6 +58,11 @@ export default async function Home() {
           <Link href="/create-room">Create Room</Link>
         </Button>
       </div>
+
+      <div className="mb-8">
+        <SearchBar />
+      </div>
+
       <div className="grid grid-cols-3 gap-4">
         {rooms.map((room) => {
           return <RoomCard key={room.id} room={room} />;
