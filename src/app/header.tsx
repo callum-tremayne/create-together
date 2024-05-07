bb67e82dffbfb9dcbbf8bd1c960b7c91e1fd4adf
tremayne97 <callum.tremayne@gmail.com>
2024-05-07 14:55:36 +0100

    video=player and header updates

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -6,11 +6,9 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { FoldHorizontalIcon, LogOutIcon } from "lucide-react";
+import { FoldHorizontalIcon, LogInIcon, LogOutIcon } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
@@ -18,7 +16,6 @@ import Link from "next/link";
 
 function AccountDropdown() {
   const session = useSession();
-  const isLoggedIn = !!session.data;
 
   return (
     <DropdownMenu>
@@ -37,32 +34,21 @@ function AccountDropdown() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        {isLoggedIn ? (
-          <DropdownMenuItem onClick={() => signOut()}>
-            <LogOutIcon className="mr-2" />
-            Sign Out
-          </DropdownMenuItem>
-        ) : (
-          <DropdownMenuItem onClick={() => signIn("google")}>
-            <LogOutIcon className="mr-2" />
-            Sign In
-          </DropdownMenuItem>
-        )}
+        <DropdownMenuItem onClick={() => signOut({ callbackUrl: "/" })} className="cursor-pointer">
+          <LogOutIcon className="mr-2" />
+          Sign Out
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
 }
 
 export function Header() {
+  const session = useSession();
   return (
     <header className="bg-gray-100 py-2 dark:bg-gray-900 mx-auto">
       <div className="container flex justify-between items-center">
-        <Link
-          href="/"
-          className="flex items-center text-xl hover:underline"
-        >
+        <Link href="/" className="flex items-center text-xl hover:underline">
           <Image
             src="/icon.png"
             className="mr-6"
@@ -70,10 +56,17 @@ export function Header() {
             height="60"
             alt="the application icon of collaborating hands on a computer"
           />
-          CREATE<FoldHorizontalIcon />TOGETHER
+          CREATE
+          <FoldHorizontalIcon />
+          TOGETHER
         </Link>
         <div className="flex items-center gap-4">
-          <AccountDropdown />
+          {session.data && <AccountDropdown />}
+          {!session.data && (
+            <Button onClick={() => signIn("google")} variant="link">
+              <LogInIcon className="mr-2" /> Sign In
+            </Button>
+          )}
           <ModeToggle />
         </div>
       </div>
--- src/app/rooms/[roomId]/video-player.tsx
+++ src/app/rooms/[roomId]/video-player.tsx
@@ -4,6 +4,7 @@ import { Room } from "@/db/schema";
 import {
   Call,
   CallControls,
+  CallParticipantsList,
   SpeakerLayout,
   StreamCall,
   StreamTheme,
@@ -14,6 +15,7 @@ import "@stream-io/video-react-sdk/dist/css/styles.css";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { generateTokenAction } from "./actions";
+import { useRouter } from "next/navigation";
 
 const apiKey = process.env.NEXT_PUBLIC_GET_STREAM_API_KEY!;
 
@@ -21,6 +23,7 @@ export function CreateTogetherVideo({ room }: { room: Room }) {
   const session = useSession();
   const [client, setClient] = useState<StreamVideoClient | null>(null);
   const [call, setCall] = useState<Call | null>(null);
+  const router = useRouter();
 
   useEffect(() => {
     if (!room) return;
@@ -30,7 +33,11 @@ export function CreateTogetherVideo({ room }: { room: Room }) {
     const userId = session.data.user.id;
     const client = new StreamVideoClient({
       apiKey,
-      user: { id: userId },
+      user: {
+        id: userId,
+        name: session.data.user.name ?? undefined,
+        image: session.data.user.image ?? undefined,
+      },
       tokenProvider: () => generateTokenAction(),
     });
     const call = client.call("default", room.id);
@@ -39,8 +46,10 @@ export function CreateTogetherVideo({ room }: { room: Room }) {
     setCall(call);
 
     return () => {
-      call.leave();
-      client.disconnectUser();
+      call
+        .leave()
+        .then(() => client.disconnectUser())
+        .catch(console.error);
     };
   }, [session, room]);
 
@@ -51,7 +60,8 @@ export function CreateTogetherVideo({ room }: { room: Room }) {
         <StreamTheme>
           <StreamCall call={call}>
             <SpeakerLayout />
-            <CallControls />
+            <CallControls onLeave={() => router.push("/")} />
+            <CallParticipantsList onClose={() => undefined} />
           </StreamCall>
         </StreamTheme>
       </StreamVideo>
